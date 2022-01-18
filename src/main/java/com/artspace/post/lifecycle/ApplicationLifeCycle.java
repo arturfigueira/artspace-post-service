f5e421712cfe86bc520c381e6c7fc7215caea554
arturfigueira <45975680+arturfigueira@users.noreply.github.com>
2022-01-17 22:16:36 -0300

    Add better app name graphic at startup log

--- src/main/java/com/artspace/post/lifecycle/ApplicationLifeCycle.java
+++ src/main/java/com/artspace/post/lifecycle/ApplicationLifeCycle.java
@@ -14,13 +14,16 @@ class ApplicationLifeCycle {
   private static final String APP_NAME = "POSTS";
 
   void onStart(@Observes StartupEvent ev) {
-    log.info(".______     ______        _______.___________.    _______.        ___      .______    __");
-    log.info("|   _  \\   /  __  \\      /       |           |   /       |       /   \\     |   _  \\  |  |");
-    log.info("|  |_)  | |  |  |  |    |   (----`---|  |----`  |   (----`      /  ^  \\    |  |_)  | |  |");
-    log.info("|   ___/  |  |  |  |     \\   \\       |  |        \\   \\         /  /_\\  \\   |   ___/  |  |");
-    log.info("|  |      |  `--'  | .----)   |      |  |    .----)   |       /  _____  \\  |  |      |  |");
-    log.info("| _|       \\______/  |_______/       |__|    |_______/       /__/     \\__\\ | _|      |__|");
+    final StringBuilder appName = new StringBuilder("\n");
+    appName
+        .append(".______     ______        _______.___________.    _______.        ___      .______    __").append("\n")
+        .append("|   _  \\   /  __  \\      /       |           |   /       |       /   \\     |   _  \\  |  |").append("\n")
+        .append("|  |_)  | |  |  |  |    |   (----`---|  |----`  |   (----`      /  ^  \\    |  |_)  | |  |").append("\n")
+        .append("|   ___/  |  |  |  |     \\   \\       |  |        \\   \\         /  /_\\  \\   |   ___/  |  |").append("\n")
+        .append("|  |      |  `--'  | .----)   |      |  |    .----)   |       /  _____  \\  |  |      |  |").append("\n")
+        .append("| _|       \\______/  |_______/       |__|    |_______/       /__/     \\__\\ | _|      |__|");
 
+    log.info(appName.toString());
     log.info("The application "+APP_NAME+" is starting with profile " + ProfileManager.getActiveProfile());
   }
 
